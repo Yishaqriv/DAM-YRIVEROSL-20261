5ecc0da5ca995667407674a145caac698c35d917
Yishaq Riveros <yishaqriveros@Yishaqs-MacBook-Air-2.local>
2026-03-11 21:01:56 -0500

    Subo Clase04, Clase05 y Clase5CALCULADORAFINAL como carpetas normales

---
+++ Clase05/src/components/screens/CountScreen/CountScreen.style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const stylesCountScreen = StyleSheet.create({
+    labelText:{
+        color: "white",
+        fontSize: 24,
+    },
+    button:{
+        backgroundColor: "purple",
+        borderRadius: 20,
+        padding: 10,
+    }
+})
---
+++ Clase05/src/components/screens/CountScreen/CountScreen.tsx
@@ -0,0 +1,20 @@
+import { Pressable, Text } from "react-native";
+import { stylesCountScreen } from "./CountScreen.style";
+import React from "react";
+
+type CountScreenProps = {
+    label: string;
+    OnPress: () => void;
+}
+
+export const CountScreen: React.FC<CountScreenProps> = (
+    { label, OnPress }
+) => {
+    return(
+        <Pressable style={stylesCountScreen.button} onPress={OnPress}>
+            <Text style={stylesCountScreen.labelText}>
+                {label}
+            </Text>
+        </Pressable>
+    );
+};
